1e95a256bbc010a053cb9fffb4ae8a24169ed4df
Daniel Toba <danieloloruntoba681@gmail.com>
2022-06-21 20:19:47 +0100

    Uncommented some code

--- index.ts
+++ index.ts
@@ -1,60 +1,60 @@
 // const firstname: string = "Daniel";
 // console.log(firstname);
-// const greeting = (lastname: string): string => {
-//   return "Hello" + lastname;
-// };
-// console.log(greeting("Toba"));
+const greeting = (lastname: string): string => {
+  return "Hello" + lastname;
+};
+console.log(greeting("Toba"));
 
-// interface UsersInterface {
-//   firstname: string;
-//   lastname: string;
-//   age: number;
-//   height: string;
-//   isActive?: boolean;
-//   getUserData?(): string;
-// }
+interface UsersInterface {
+  firstname: string;
+  lastname: string;
+  age: number;
+  height: string;
+  isActive?: boolean;
+  getUserData?(): string;
+}
 
-// const userData: UsersInterface = {
-//   firstname: "Daniel",
-//   lastname: "Oloruntoba",
-//   age: 19,
-//   height: "5'11",
-//   isActive: true,
-//   getUserData() {
-//     return `Username is ${this.firstname} and the user age is ${
-//       this.age
-//     }. \nUser's height is ${this.height} and the user is ${
-//       this.isActive ? "active" : "not active"
-//     }`;
-//   },
-// };
-// const user2: UsersInterface = {
-//   firstname: "Karly",
-//   lastname: "Divir",
-//   age: 19,
-//   height: "5'10",
-// };
-// console.log(user2.firstname + " " + user2.lastname);
-// console.log(userData.getUserData());
+const userData: UsersInterface = {
+  firstname: "Daniel",
+  lastname: "Oloruntoba",
+  age: 19,
+  height: "5'11",
+  isActive: true,
+  getUserData() {
+    return `Username is ${this.firstname} and the user age is ${
+      this.age
+    }. \nUser's height is ${this.height} and the user is ${
+      this.isActive ? "active" : "not active"
+    }`;
+  },
+};
+const user2: UsersInterface = {
+  firstname: "Karly",
+  lastname: "Divir",
+  age: 19,
+  height: "5'10",
+};
+console.log(user2.firstname + " " + user2.lastname);
+console.log(userData.getUserData());
 
 // //Unions
-// let favNumber: string | number = 12;
-// console.log(favNumber);
-// let errorMessage: string | null = null;
-// if (userData.firstname !== "Daniel") {
-//   errorMessage = "Unidentified user";
-//   console.log(errorMessage);
-// } else {
-//   console.log("Welcome back " + userData.firstname);
-// }
+let favNumber: string | number = 12;
+console.log(favNumber);
+let errorMessage: string | null = null;
+if (userData.firstname !== "Daniel") {
+  errorMessage = "Unidentified user";
+  console.log(errorMessage);
+} else {
+  console.log("Welcome back " + userData.firstname);
+}
 
 // //Types
-// type Fruits = string;
+type Fruits = string;
 
-// const fruits: Fruits[] = ["Mango", "Banana", "Orange"];
-// for (let i = 0; i < fruits.length; i++) {
-//   console.log(fruits[i]);
-// }
+const fruits: Fruits[] = ["Mango", "Banana", "Orange"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
 const greetButton: HTMLElement = document.getElementById("greetMe");
 const greetUser = document.getElementById("username");
 const firstname = document.getElementById("firstname") as HTMLInputElement;
@@ -67,31 +67,31 @@ greetButton.addEventListener("click", () => {
   }
 });
 
-// const addId = <T>(obj: T) => {
-//   const id = Math.random().toString(16);
-//   return {
-//     ...obj,
-//     id,
-//   };
-// };
+const addId = <T>(obj: T) => {
+  const id = Math.random().toString(16);
+  return {
+    ...obj,
+    id,
+  };
+};
 
-// const user = {
-//   name: "Daniel",
-// };
-// const result = addId(user);
-// console.log(result);
-// interface FriendInterface {
-//   fullname: string;
-//   age: number;
-//   is_friend_closeby: boolean;
-// }
+const user = {
+  name: "Daniel",
+};
+const result = addId(user);
+console.log(result);
+interface FriendInterface {
+  fullname: string;
+  age: number;
+  is_friend_closeby: boolean;
+}
 
-// const user1: FriendInterface = {
-//   fullname: "Karly Divirgilio",
-//   age: 19,
-//   is_friend_closeby: false,
-// };
-// console.log(user1.is_friend_closeby);
+const user1: FriendInterface = {
+  fullname: "Karly Divirgilio",
+  age: 19,
+  is_friend_closeby: false,
+};
+console.log(user1.is_friend_closeby);
 
 //Enums
 //Values in enums are written with an "=" sign
